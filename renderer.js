f67434943a009dad2be39569f77371a5a397dcb1
Steve Ballantine <Stephen@51degrees.com>
2022-04-09 04:53:40 +0100

    AUTO: Update scripts from latest data

--- renderer.js
+++ renderer.js
@@ -28,7 +28,7 @@
 // @param complete the function to call with the renderer.
 // @param takUrl the full URL of the tak.js file (optional).
 function getRenderer(complete, takUrl) {
-	/*! VERSION = 1.620211 */ 
+	/*! VERSION = 1.620581 */ 
 	var nodes = 
 		[
 		  {
