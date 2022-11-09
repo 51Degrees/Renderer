9fefed68efd2227cce2a6e651981888ae7a3cb28
Sylvia User <SylviaUser@51degrees.com>
2022-11-09 00:22:16 +0000

    AUTO: Update scripts from latest data

--- renderer.js
+++ renderer.js
@@ -32,7 +32,7 @@ function getRenderer(complete, takUrl) {
 	var decisionTree = 
 		{
 		  "Version": "1.641563",
-		  "PublishDate": "2022-11-08T00:26:58.7457260Z",
+		  "PublishDate": "2022-11-09T00:21:31.2118144Z",
 		  "Data": [
 		    {
 		      "x": "Unknown",
@@ -396,7 +396,7 @@ function getRenderer(complete, takUrl) {
 		    },
 		    {
 		      "x": "Apple A14 GPU|Apple A15 GPU|Apple M2 GPU",
-		      "m": function(n){return width(n);},
+		      "m": function(n){return ratio(n);},
 		      "n": [
 		        81,
 		        80
@@ -888,13 +888,13 @@ function getRenderer(complete, takUrl) {
 		        57
 		      ],
 		      "v": [
-		        1284
+		        3.0
 		      ]
 		    },
 		    {
 		      "x": "Apple M2 GPU",
 		      "v": [
-		        1940
+		        2.0
 		      ]
 		    },
 		    {
