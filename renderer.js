4d12a52874d2e3b42aba85ed7aa8fe18cab4259f
Sylvia User <SylviaUser@51degrees.com>
2023-01-20 00:22:55 +0000

    AUTO: Update scripts from latest data

--- renderer.js
+++ renderer.js
@@ -27,7 +27,7 @@ function getRenderer(complete, takUrl) {
 	var decisionTree = 
 		{
 		  "Version": "1.641563",
-		  "PublishDate": "2023-01-19T00:21:41.5427050Z",
+		  "PublishDate": "2023-01-20T00:21:47.7797123Z",
 		  "Data": [
 		    {
 		      "x": "Unknown",
